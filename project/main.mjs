672895709b281346661ce7fbd5cec7a911d5c1a1
Henrik Kniberg <h@kniberg.com>
2023-08-25 08:04:49 +0200

    Added some more challenging test prompts

--- project/main.mjs
+++ project/main.mjs
@@ -15,12 +15,14 @@ resetFolder(path.join(OUTPUT_FOLDER, "files"));
 
 const MODEL = "gpt-4";
 (async () => {
+    /*
     let prompt1 = "Create a file on my computer called ../output/files/hi.txt which contains the text 'Hello World!'"
 
     let prompt2 = "Download the contents of http://kniberg.com and save to a file called ../output/files/kniberg.txt."
 
     let prompt3 = "Clone the github repo https://github.com/hkniberg/test-project  " +
         "Run it and tell me what the output is."
+        */
 
     let prompt4 = "What does https://github.com/hkniberg/test-project do?"
 
